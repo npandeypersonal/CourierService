99ea3da0ed9e3789c1f2c117a743c985212b6a1a
Nand lal Pandey <pandey.nand11@gmail.com>
2024-01-12 03:21:25 +0530

    Refactored util class.

--- app/src/main/java/com/courierservice/challenge/utils/Utils.kt
+++ app/src/main/java/com/courierservice/challenge/utils/Utils.kt
@@ -5,6 +5,7 @@ import com.courierservice.challenge.data.database.entities.PackageDetailsEntity
 import com.courierservice.challenge.data.models.PackageDetails
 import com.courierservice.challenge.data.models.VehicleDetails
 import java.math.RoundingMode
+import kotlin.math.max
 
 object Utils {
 
@@ -28,45 +29,57 @@ object Utils {
         return calculateDeliveryEstimation(sortedPackages,vehicleAvailability,vehicleDetails, mutableListOf())
     }
     private fun calculateDeliveryEstimation(packages: List<PackageDetailsEntity>, vehicleAvailability: MutableList<Double>, vehicleDetails: VehicleDetails, shipments: MutableList<PackageDetails>): MutableList<PackageDetails> {
-        var currentWeight = 0.0
         var currentDuration = vehicleAvailability.min()
         val vehicleIndex = vehicleAvailability.indexOf(currentDuration)
         val pkgSize = packages.size
         if (pkgSize <= 1){
             if (pkgSize == 1) {
                 val deliveryTime = packages[0].pkgDistance / vehicleDetails.maxSpeed
-                currentWeight += packages[0].pkgWeight
-                val totalTime = currentDuration + deliveryTime
-                val roundedUp = totalTime.toBigDecimal().setScale(2, RoundingMode.DOWN).toDouble()
+                currentDuration += deliveryTime
+                val roundedUp = currentDuration.toBigDecimal().setScale(2, RoundingMode.DOWN).toDouble()
                 shipments.add(PackageDetails(packages[0].pkgId, "", "", roundedUp.toString()))
             }
             return shipments
         }
-        var newPkgList = packages
+        val minDiff = getMinDifference(packages,vehicleDetails.maxCarriableWeight)
+        val (newPkgList,newShipments,currentTime) = getDeliveriesAndRemainingShipments(packages,vehicleDetails,minDiff,shipments,currentDuration)
 
+        vehicleAvailability[vehicleIndex] = 2*currentTime
+        return calculateDeliveryEstimation(newPkgList,vehicleAvailability,vehicleDetails,newShipments)
+    }
+    private fun getMinDifference(packages: List<PackageDetailsEntity>, maxCarriableWeight: Double): Double{
         var start = 0
         var end = 0
         var currSum: Double = packages[0].pkgWeight
-        var minDiff = vehicleDetails.maxCarriableWeight - currSum
+        var minDiff = maxCarriableWeight - currSum
 
+        val pkgSize = packages.size
         while (end < pkgSize - 1) {
-            if (currSum < vehicleDetails.maxCarriableWeight) {
+            if (currSum < maxCarriableWeight) {
                 end++
                 currSum += packages[end].pkgWeight
             } else {
                 currSum -= packages[start].pkgWeight
                 start++
             }
-            if (vehicleDetails.maxCarriableWeight - currSum < minDiff && vehicleDetails.maxCarriableWeight - currSum >=0 ) {
-                minDiff = vehicleDetails.maxCarriableWeight - currSum
+            if (maxCarriableWeight - currSum < minDiff && maxCarriableWeight - currSum >=0 ) {
+                minDiff = maxCarriableWeight - currSum
             }
         }
-        start = 0
-        end = 0
-        var currentIndex = 0
-        currSum = packages[0].pkgWeight
-        while (end < pkgSize - 1) {
+        return minDiff
+    }
 
+    private fun getDeliveriesAndRemainingShipments(packages: List<PackageDetailsEntity>, vehicleDetails: VehicleDetails, minDiff:Double,
+                                                   shipments: MutableList<PackageDetails>, currentTime: Double): Triple<List<PackageDetailsEntity>,MutableList<PackageDetails>,Double>{
+        var start = 0
+        var end = 0
+        var currentIndex: Int
+        var currSum = packages[0].pkgWeight
+        val pkgSize = packages.size
+        var newPkgList = packages.toMutableList()
+        var currentWeight = 0.0
+        var currentDuration = currentTime
+        while (end < pkgSize - 1) {
             if (currSum < vehicleDetails.maxCarriableWeight) {
                 end++
                 currSum += packages[end].pkgWeight
@@ -76,38 +89,25 @@ object Utils {
                 currSum -= packages[start].pkgWeight
                 start++
             }
-            if (vehicleDetails.maxCarriableWeight - currSum == minDiff) {
-                var tempCurrentTime = 0.0
-                for (i in start .. end) {
-                    val deliveryTime = packages[i].pkgDistance/vehicleDetails.maxSpeed
-                    currentWeight += packages[i].pkgWeight
-                    val totalTime = currentDuration + deliveryTime
-                    val roundedUp = totalTime.toBigDecimal().setScale(2, RoundingMode.DOWN).toDouble()
-                    tempCurrentTime = Math.max(tempCurrentTime,roundedUp)
-                    shipments.add(PackageDetails(packages[i].pkgId,"","",roundedUp.toString()))
-                    newPkgList = newPkgList.filter { it.pkgId != packages[i].pkgId }
+            if (vehicleDetails.maxCarriableWeight - currSum == minDiff ||
+                (currSum > vehicleDetails.maxCarriableWeight && vehicleDetails.maxCarriableWeight - (currSum - packages[currentIndex].pkgWeight) == minDiff)) {
+                if (currSum > vehicleDetails.maxCarriableWeight){
+                    end--
                 }
-                currentDuration += tempCurrentTime
-                break
-            }else if (currSum > vehicleDetails.maxCarriableWeight && vehicleDetails.maxCarriableWeight - (currSum - packages[currentIndex].pkgWeight) == minDiff){
                 var tempCurrentTime = 0.0
-                end--
                 for (i in start .. end) {
                     val deliveryTime = packages[i].pkgDistance/vehicleDetails.maxSpeed
                     currentWeight += packages[i].pkgWeight
                     val totalTime = currentDuration + deliveryTime
                     val roundedUp = totalTime.toBigDecimal().setScale(2, RoundingMode.DOWN).toDouble()
-                    tempCurrentTime = Math.max(tempCurrentTime,roundedUp)
+                    tempCurrentTime = max(tempCurrentTime,roundedUp)
                     shipments.add(PackageDetails(packages[i].pkgId,"","",roundedUp.toString()))
-                    newPkgList = newPkgList.filter { it.pkgId != packages[i].pkgId }
+                    newPkgList.remove(packages[i])
                 }
                 currentDuration += tempCurrentTime
                 break
             }
-
         }
-
-        vehicleAvailability[vehicleIndex] = 2*currentDuration
-        return calculateDeliveryEstimation(newPkgList,vehicleAvailability,vehicleDetails,shipments)
+        return Triple(newPkgList,shipments,currentDuration)
     }
 }
